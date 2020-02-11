cc4ec0d60b24773b373c0ee6543ec6af2fed1b53
Dekahri Jones <JonesDD1@cougars.sf.edu>
2020-02-11 11:00:20 -0500

    Start

---
+++ stringManipulation/stringManipulation/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace stringManipulation
+{
+   class Program
+   {
+      static void Main(string[] args)
+      {
+         string sentence = "The quick brown fox jumped over the lazy dog";
+         string convertedSentence = "";
+
+         Console.WriteLine(sentence);
+
+         Console.WriteLine(sentence.ToLower());
+         Console.WriteLine(sentence.ToUpper());
+
+         Console.WriteLine($"Character count: {sentence.Length}");
+         Console.WriteLine($"Does the sentence contain the word 'fox'? {sentence.Contains("fox")}");
+
+
+         Console.WriteLine($"Index of 'q': {sentence.IndexOf("q")}");
+
+         Console.WriteLine($"Extract the word 'fox': {sentence.Substring(sentence.IndexOf('f'), 3)}");
+      }
+   }
+}
